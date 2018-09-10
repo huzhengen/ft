56001c4eb74143be0f394e766f83707e62221e3b
huzhengen <huzhengen@163.com>
2018-09-10 09:21:45 +0800

    enter.php

--- enter.php
+++ enter.php
@@ -5,3 +5,17 @@
  * Date: 2018-09-07
  * Time: 15:25:06
  */
+include "vendor/autoload.php";
+
+$m = v('m') ? v('m') : 'resume';
+$m = ucfirst(strtolower($m));
+$a = v('a') ? v('a') : 'index';
+
+
+try{
+	$controller = "FranFrame\\Contorller\\".$m;
+	call_user_func([new $m(), a]);
+//	throw new Exception("error");
+}catch(\Exception $e){
+	die($e->getMessage());
+}
